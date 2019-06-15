040925c7ff99a72eee042f689854a0ee6847cd4e
AndrewGil <coolman357@gmail.com357>
2019-06-15 12:08:00 -0700

    assembler start

--- src/μcompile/assembler.rs
+++ src/μcompile/assembler.rs
@@ -31,7 +31,7 @@ pub enum AssemblerLine {
 	Label(String),
 	Goto(String)
 }
-impl ToString for AssemblerLine {
+impl ToString for ExecutableLine {
 	fn to_string(&self) -> String {
 		self.opcode.to_string() + " " + self.output.to_string() + " "
 			+ self.arg1.to_string() + " " + self.arg2.to_string()
@@ -55,27 +55,42 @@ struct Assembler<RawOut: std::io::Write> {
 	lines: VecDeque<AssemblerLine>,
 	labels: HashMap<String, u16>,
 	unknown_labels: Vec<(String, u16)>,
+	pc: u16,
 	out: RawOut
 }
 impl<RawOut: std::io::Write> Assembler<RawOut> {
+	pub fn new(byte_out: RawOut) -> Assembler<RawOut> {
+		Assembler {
+			out: byte_out,
+			..Default()
+		}
+	}
 	fn next_line(&mut self) -> Option<AssemblerLine> {
 		self.lines.pop_front()
 	}
 	fn add_label(&mut self, label: String) -> Option<()> {
 
 	}
+	fn find_label(&self, label: &str) -> Option<u16> {
+		Some(*self.labels.iter().find(|li: &(String, u16)|{
+			return (li.0) == label;
+		})?.1)
+	}
 	fn process_executable(&mut self, executeable: ExecutableLine) -> Option<()> {
 
 	}
 	fn process_goto(&mut self, label: String) -> Option<()> {
-
+		let line_number
+		self.process_executable()
 	}
-
-	pub fn process_line(&mut self) -> Option<()> {
-		match self.current_line()? {
+	fn process_line(&mut self, line: AssemblerLine) -> Option<()> {
+		match line {
 			AssemblerLine::Label(label) => self.add_label(label),
 			AssemblerLine::Executable(executable) => self.process_executable(executable),
 			AssemblerLine::Goto(label) => self.process_goto(label)
 		}
+	}
+	pub fn process_next(&mut self) -> Option<()> {
+		self.process_line(self.next_line()?)
 	}
 }
